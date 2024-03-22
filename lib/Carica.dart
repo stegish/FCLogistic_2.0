dd2ea10906d9c232d282ac511aee4d9d2c02eed5
Michael Fantinato <fantinato.michael@gmail.com>
2024-03-22 15:02:02 +0100

    creata carica.dart e quasi completata la sua pagina php

--- lib/Carica.dart
+++ lib/Carica.dart
@@ -12,15 +12,16 @@ class Carica extends StatefulWidget {
 class _CaricaState extends State<Carica> {
 
   static final GlobalKey<ScaffoldState> _Carica = GlobalKey<ScaffoldState>(); //key per i pop-up
-  final input = [TextEditingController()]; //bancale
+  final input = [TextEditingController(),  TextEditingController()]; //bancale
+  String bancale="";
 
   Future<List<String>> RealTimeSearch(String cod) async{
     List<String> risultato = [];
-    var url = "http://188.12.130.133:1717/Trova.php";
+    var url = "http://188.12.130.133:1717/TrovaBancale.php";
     http.Response response = await http.post(
       Uri.parse(url),
       body: {
-        'codice': cod,
+        'bancale': cod,
       },
     );
     var responseD = jsonDecode(response.body);
@@ -34,6 +35,27 @@ class _CaricaState extends State<Carica> {
       }
     }
     return risultato;
+  }
+
+  void SendData() async{
+    var url = "http://188.12.130.133:1717/Carica.php";
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: {
+        'codice': input[0].text,
+        'bancale': bancale,
+        'quantita': input[1].text,
+        'data': DateTime.now().year.toString() +"-"+DateTime.now().month.toString() +"-"+ DateTime.now().day.toString(),
+        'descrizione': '',
+      },
+    );
+    var responseD = jsonDecode(response.body);
+    print(responseD);
+    if(responseD['success']==true){
+      print("successo");
+    }else{
+      print(responseD);
+    }
   }
 
   VaiCVMagazzino(){
@@ -58,11 +80,39 @@ class _CaricaState extends State<Carica> {
               child : Autocomplete<String>(
                 optionsBuilder: (TextEditingValue textEditingValue) async {
                   List<String> ris = await RealTimeSearch(textEditingValue.text);
+                  bancale=textEditingValue.text;
                   return ris;
                   },
                 onSelected: (String selection) {
-                  debugPrint('You just selected $selection');
                 },
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(30.0),
+              child : TextFormField(
+                keyboardType: TextInputType.number,
+                controller: input[0],
+                decoration: const InputDecoration(
+                  hintText: 'inserire il codice',
+                  labelText: 'codice *',
+                ),
+                validator: (val) => (val!.isEmpty||val.contains('.')||val.length!=8||val.contains(',')||val.contains('-')||val.contains(' ')) ? "togli \". , -\" e spazi, max 8 cifre" : null,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: input[1],
+                  decoration: const InputDecoration(
+                    hintText: 'inserire quantita',
+                    labelText: 'quantita *',
+                  ),
+                  validator: (val){
+                    if(val!.isEmpty || val.length>8){
+                      return "inserisci quantita";
+                    }else{
+                      return null;
+                    }}
               ),
             ),
           ],
@@ -70,7 +120,7 @@ class _CaricaState extends State<Carica> {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.red,
-        onPressed: VaiCVMagazzino,
+        onPressed: SendData,
         tooltip: 'ricerca',
         child: const Icon(Icons.search),
       ),
