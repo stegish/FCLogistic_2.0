b85f7012ae9b6ede07e6b1b28855076c8c28af75
stegish <jampista.fantinato@gmail.com>
2024-04-01 22:34:40 +0200

    grafico creato per home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -20,75 +20,85 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home>{
-  final List<FlSpot> dummyData1 = List.generate(8, (index) {
-    return FlSpot(index.toDouble(), index * Random().nextDouble());
-  });
+  List<FlSpot> resi= [];
+  List<FlSpot> impegnati = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
 
   Future<int> getResiFromMounth(String mese) async {
     http.Response response = await http.post(
       Uri.parse('http://188.12.130.133:1717/MeseReso.php'),
     body: {
-        'data' : mese,
+        'mese' : mese,
     });
     var responseD = jsonDecode(response.body);
-    var data = responseD['data'];
-    return data.length;
+    if(responseD['success']==true){
+      var data = responseD['data'];
+      return data.length;
+    }else{
+      throw Exception(responseD['error']);
+    }
   }
 
   Future<int> getImpegnatiFromMounth(String mese) async {
     http.Response response = await http.post(
         Uri.parse('http://188.12.130.133:1717/MeseImpegnati.php'),
         body: {
-          'data' : mese,
+          'mese' : mese,
         });
     var responseD = jsonDecode(response.body);
-    var data = responseD['data'];
-    return data.length;
+    if(responseD['success']==true){
+      var data = responseD['data'];
+      return data.length;
+    }else{
+      throw Exception(responseD['error']);
+    }
+
   }
 
 
-  void getData(){
-    for(int i=0; i<4; i++){
-
+  Future<void> getData() async {
+    DateTime mese = DateTime.now();
+    for(int i=0; i<12; i++){
+      int meser =  await getResiFromMounth(mese.month.toString());
+      int mesei = await getResiFromMounth(mese.month.toString());
+      resi.add(FlSpot(i.toDouble(), meser.toDouble()));
+      impegnati.add(FlSpot(i.toDouble(), mesei.toDouble()));
+      mese = DateTime(mese.year, mese.month-1, mese.day);
     }
-    FlSpot()
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          width: double.infinity,
-          child: LineChart(
-            LineChartData(
-              borderData: FlBorderData(show: false),
-              lineBarsData: [
-                // The red line
-                LineChartBarData(
-                  spots: dummyData1,
-                  isCurved: true,
-                  barWidth: 3,
-                  color: Colors.indigo,
-                ),
-                // The orange line
-                LineChartBarData(
-                  spots: dummyData2,
-                  isCurved: true,
-                  barWidth: 3,
-                  color: Colors.red,
-                ),
-                // The blue line
-                LineChartBarData(
-                  spots: dummyData3,
-                  isCurved: false,
-                  barWidth: 3,
-                  color: Colors.blue,
-                )
-              ],
-            ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        width: double.infinity,
+        child: LineChart(
+          LineChartData(
+            borderData: FlBorderData(show: false),
+            lineBarsData: [
+              // The red line
+              LineChartBarData(
+                spots: resi,
+                isCurved: true,
+                barWidth: 3,
+                color: Colors.indigo,
+              ),
+              // The orange line
+              LineChartBarData(
+                spots: impegnati,
+                isCurved: true,
+                barWidth: 3,
+                color: Colors.red,
+              ),
+            ],
           ),
         ),
       ),
